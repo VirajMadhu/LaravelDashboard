e1ef435db7caa8eea755052575de249565e6137c
VirajMadhu <viraj.madhushan.971210@gmail.com>
2022-03-18 15:38:06 +0530

    #3 Add footer & custom dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,35 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Products') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <x-show-products />
+@role('super_admin')
+    <x-admin-layout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</x-app-layout>
+    </x-admin-layout>
+@else
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Products') }}
+            </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <x-show-products />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <x-slot name="footer">
+            <x-footer />
+        </x-slot>
+    </x-app-layout>
+@endrole
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,20 @@
         @include('layouts.navigation')
 
         <!-- Page Heading -->
-        <header class="bg-white shadow">
+        {{-- <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
-        </header>
+        </header> --}}
 
         <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
+
+        <footer>
+            {{ $footer }}
+        </footer>
     </div>
 </body>
 
